86cdd4c65729ba0d4ec1f268a61caef27648cfe2
yoshiharahyui <1015hyui@gmail.com>
2024-07-03 18:36:36 +0900

    first-commit

---
+++ MeigenApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MeigenApp
+//
+//  Created by 吉原飛偉 on 2024/07/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
